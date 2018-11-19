af412e3b3028e9452aa15b300473275fd31b2af4
animus apertus <37455165+ethantatum@users.noreply.github.com>
2018-11-19 16:07:35 -0700

    added image and finalized basic functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,3 +12,41 @@ let guessesLeft = 3;
 let wins = 0;
 let losses = 0;
 
+computerGuess = computerBank[Math.floor(Math.random() * computerBank.length)];
+console.log(computerGuess);
+
+// FUNCTIONS
+// =================================
+function newgame() {
+    guessesLeft = 3;
+    userGuesses = [];
+    computerGuess = computerBank[Math.floor(Math.random() * computerBank.length)];
+    console.log(computerGuess);
+    guessesLeftText.innerHTML = guessesLeft;
+    userGuessesText.innerHTML = userGuesses.join(' ');
+}
+
+document.onkeyup = function(event) {
+    userGuess = event.key;
+    if(userGuess == computerGuess) {
+        alert("Whoa...you must be psychic!");
+        wins++;
+        newgame()
+    } else if 
+        (userGuess >= 1 && userGuess <= 9) {
+            userGuesses.push(userGuess);
+            userGuessesText.innerHTML = userGuesses.join(' ');
+            guessesLeft--;
+            if(guessesLeft === 0) {
+                alert('Yikes...no psychic here...');
+                losses++;
+                newgame();
+            }
+        } else {
+        alert("Wow...picking a letter...not a good start! Choose a number!");
+    }
+
+    guessesLeftText.innerHTML = guessesLeft;
+    winsText.innerHTML = wins;
+    lossesText.innerHTML = losses;
+}
